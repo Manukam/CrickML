ab1f60d146587363c1880fe180876b1a9b748b06
manukam <maduranga.manuka3@gmail.com>
2019-02-27 03:31:37 +0530

    setting up

--- db_connector.py
+++ db_connector.py
@@ -14,7 +14,7 @@ try:
     #     sql = "INSERT INTO `users` (`email`, `password`) VALUES (%s, %s)"
     #     cursor.execute(sql, ('webmaster@python.org', 'very-secret'))
 
-    # # connection is not autocommit by default. So you must commit to save
+    # # connection is n ot autocommit by default. So you must commit to save
     # # your changes.
     # connection.commit()
 
