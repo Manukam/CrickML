734e370a06b8786ed6d7dbf263b925dbffdfab0f
manukam <maduranga.manuka3@gmail.com>
2019-03-06 01:20:40 +0530

    progress

--- regression.py
+++ regression.py
@@ -41,13 +41,13 @@ try:
             home_score = batsmen_model(player['home_matches'], player['home_innings'],
                                        player['home_average'], player['home_100s'], player['home_50s'])
 
-            condition_performances = away_score + home_score
+            # condition_performances = away_score + home_score
 
-            career_score = career_score + condition_performances
+            # career_score = career_score + condition_performances
 
-            batsmen_score = (0.35 * career_score) + (0.65 * player['form_average'] )
+            # batsmen_score = (0.35 * career_score) + (0.65 * player['form_average'] )
 
-            player_list.append([batsmen_score])
+            player_list.append([career_score,away_score,home_score])
 
     with connection.cursor() as cursor:
         sql = "SELECT * FROM `pre_wc_2015`"
@@ -64,36 +64,36 @@ try:
             home_score = batsmen_model(player['home_matches'], player['home_innings'],
                                        player['home_average'], player['home_100s'], player['home_50s'])
 
-            condition_performances = away_score + home_score
+            # condition_performances = away_score + home_score
 
-            career_score = career_score + condition_performances
+            # career_score = career_score + condition_performances
 
-            batsmen_score = (0.35 * career_score) + (0.65 * player['form_average'] )
+            # batsmen_score = (0.35 * career_score) + (0.65 * player['form_average'] )
 
-            player_list.append([batsmen_score])
+            player_list.append([career_score,away_score,home_score])
 
-    with connection.cursor() as cursor:
-        sql = "SELECT * FROM `pre_ct_2017`"
-        cursor.execute(sql)
-        result = cursor.fetchall()
-        # player_list = []
-        for player in result:
-            career_score = batsmen_model(player['overall_matches'], player['overall_innings'],
-                                         player['overall_average'], player['overall_100s'], player['overall_50s'])
+    # with connection.cursor() as cursor:
+    #     sql = "SELECT * FROM `pre_ct_2017`"
+    #     cursor.execute(sql)
+    #     result = cursor.fetchall()
+    #     # player_list = []
+    #     for player in result:
+    #         career_score = batsmen_model(player['overall_matches'], player['overall_innings'],
+    #                                      player['overall_average'], player['overall_100s'], player['overall_50s'])
 
-            away_score = batsmen_model(player['away_matches'], player['away_innings'],
-                                       player['away_average'], player['away_100s'], player['away_50s'])
+    #         away_score = batsmen_model(player['away_matches'], player['away_innings'],
+    #                                    player['away_average'], player['away_100s'], player['away_50s'])
 
-            home_score = batsmen_model(player['home_matches'], player['home_innings'],
-                                       player['home_average'], player['home_100s'], player['home_50s'])
+    #         home_score = batsmen_model(player['home_matches'], player['home_innings'],
+    #                                    player['home_average'], player['home_100s'], player['home_50s'])
 
-            condition_performances = away_score + home_score
+    #         # condition_performances = away_score + home_score
 
-            career_score = career_score + condition_performances
+    #         # career_score = career_score + condition_performances
 
-            batsmen_score = (0.35 * career_score) + (0.65 * player['form_average'] )
+    #         # batsmen_score = (0.35 * career_score) + (0.65 * player['form_average'] )
 
-            player_list.append([batsmen_score])
+    #         player_list.append([career_score,away_score,home_score])
 
     with connection.cursor() as cursor:
         sql = "SELECT * FROM `pre_ct_2013`"
@@ -110,13 +110,13 @@ try:
             home_score = batsmen_model(player['home_matches'], player['home_innings'],
                                        player['home_average'], player['home_100s'], player['home_50s'])
 
-            condition_performances = away_score + home_score
+            # condition_performances = away_score + home_score
 
-            career_score = career_score + condition_performances
+            # career_score = career_score + condition_performances
 
-            batsmen_score = (0.35 * career_score) + (0.65 * player['form_average'] )
+            # batsmen_score = (0.35 * career_score) + (0.65 * player['form_average'] )
 
-            player_list.append([batsmen_score])
+            player_list.append([career_score,away_score,home_score])
 
         np_players = np.array(player_list)
         # print(np_players) 
@@ -124,31 +124,32 @@ try:
         # exit()
         max_career = np.max(np_players[:, 0])
         # max_recent = np.max(np_players[:, 1])
-        # max_away = np.max(np_players[:, 2])
-        # max_home = np.max(np_players[:, 3])
-        # print(max_career)
+        max_away = np.max(np_players[:, 1])
+        max_home = np.max(np_players[:, 2])
+        print(max_career)
+        print(max_away)
+        print(max_home)
         # exit()
-        # print(max_recent)
         for player in np_players:
             career_score = player[0]
             # recent_score = player[1]
-            # away_score = player[2]
-            # home_score = player[3]
+            away_score = player[1]
+            home_score = player[2]
 
             normalized_career_score = career_score/max_career
-            player[0] = normalized_career_score / 2
+            player[0] = normalized_career_score
 
             # if(recent_score != 0):
             #     normalized_recent_score = recent_score/max_recent
             #     player[1] = normalized_recent_score
 
-            # if(away_score != 0):
-            #     normalized_away_score = away_score/max_away
-            #     player[2] = normalized_away_score
+            if(away_score != 0):
+                normalized_away_score = away_score/max_away
+                player[1] = normalized_away_score
 
-            # if(home_score != 0):
-            #     normalized_home_score = home_score/max_home
-            #     player[3] = normalized_home_score
+            if(home_score != 0):
+                normalized_home_score = home_score/max_home
+                player[2] = normalized_home_score
 
     # a_train, a_test, b_train, b_test = train_test_split(a, b, test_size=0.33, random_state=42)
 
@@ -174,16 +175,16 @@ try:
                                              player['overall_average'], player['overall_100s'], player['overall_50s'])
             performance_list.append([tournement_score])
 
-    with connection.cursor() as cursor:
-        # Read a single record
-        sql = "SELECT * FROM `ct_2017`"
-        cursor.execute(sql)
-        result = cursor.fetchall()
-        # performance_list = []
-        for player in result:
-            tournement_score = batsmen_model(player['overall_matches'], player['overall_innings'],
-                                             player['overall_average'], player['overall_100s'], player['overall_50s'])
-            performance_list.append([tournement_score])
+    # with connection.cursor() as cursor:
+    #     # Read a single record
+    #     sql = "SELECT * FROM `ct_2017`"
+    #     cursor.execute(sql)
+    #     result = cursor.fetchall()
+    #     # performance_list = []
+    #     for player in result:
+    #         tournement_score = batsmen_model(player['overall_matches'], player['overall_innings'],
+    #                                          player['overall_average'], player['overall_100s'], player['overall_50s'])
+    #         performance_list.append([tournement_score])
 
     with connection.cursor() as cursor:
         # Read a single record
@@ -213,7 +214,7 @@ try:
         normalized_tournement_score = tourney_score/max_tournament_score
 
         # print(normalized_tournement_score)
-        player[0] = normalized_tournement_score / 2
+        player[0] = normalized_tournement_score
 
 finally:
     connection.close()
@@ -244,8 +245,8 @@ feature_train_for_plot = feature_train[:, 0]
 target_test_for_plot = target_test[:,0]
 target_train_for_plot = target_train[:,0]
 
-# print(feature_train_for_plot)
-# print(feature_test_for_plot)
+print(feature_train_for_plot)
+print(feature_test_for_plot)
 # print(target_train)
 # exit()
 for feature, target in np.nditer([feature_test_for_plot, target_test_for_plot]):
@@ -255,10 +256,10 @@ for feature, target in np.nditer([feature_train_for_plot, target_train_for_plot]
 
 # print(target_test[0])
 # labels for the legend
-plt.scatter(feature_test_for_plot[0],
-            target_test_for_plot[0], color=test_color, label="test")
-plt.scatter(feature_test_for_plot[0],
-            target_test_for_plot[0], color=train_color, label="train")
+# plt.scatter(feature_test_for_plot[0],
+#             target_test_for_plot[0], color=test_color, label="test")
+# plt.scatter(feature_test_for_plot[0],
+#             target_test_for_plot[0], color=train_color, label="train")
 
 
 # draw the regression line, once it's coded
